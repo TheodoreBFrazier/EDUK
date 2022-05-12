d5b207bb7352461ef9b6d35340aeb545e6c95f1d
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-12 15:43:05 -0400

    Started building navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+
 import "./App.css";
 
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div className="App">HELLO WORLD!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,16 @@
+//importing Link function
+
+import { Link } from "react-router-dom"
+
+export default function NavBar() {
+    return (
+        <nav className="navigation">
+            <Link to="/">
+                <h3> NYC Rose 🌹</h3>
+            </Link>
+            <button>
+                New User
+            </button>
+        </nav>
+    )
+}
