fe4457526ebe3c118ce5cde0995d373ed7dec6b7
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-12 16:27:16 -0400

    Starting single user view

---
+++ frontend/src/components/SingleUserView.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function SingleUser({ user }) {
+    return (
+        <div className="useruserview">
+            <Link >
+
+            </Link>
+        </div>
+    )
+}
+
+export default SingleUser
