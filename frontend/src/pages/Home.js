804189ddf33a4a863c3101f36a31debcc334ce64
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-14 15:58:51 -0400

    Added an image - this is just a test image - we need permission to us

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,6 +7,7 @@ function Home() {
                 <h1>EVENING THE PLAYING FIELD</h1>
                 <h3>LINKING NYERS TO ESSENTIAL SERVICES  </h3>
             </div>
+            <img src="https://cdn.dribbble.com/users/1626229/screenshots/14556747/media/ceb42cd2838a68f02e27032469c01870.jpg" />
         </div>
     )
 }
