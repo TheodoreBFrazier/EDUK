c330f973a57c197f520f8c5ff2b0012383f71441
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-14 17:26:46 -0400

    Building out the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,11 +3,17 @@ import React from "react";
 function Home() {
     return (
         <div className="Home">
-            <div className="Hero-text">
-                <h1>EVENING THE PLAYING FIELD</h1>
-                <h3>LINKING NYERS TO ESSENTIAL SERVICES  </h3>
+            <div className="Hero">
+                <div className="Hero-text">
+                    <h1>EVENING THE PLAYING FIELD</h1>
+                    <h3>LINKING NYERS TO ESSENTIAL SERVICES  </h3>
+                </div>
             </div>
-            <img src="https://cdn.dribbble.com/users/1626229/screenshots/14556747/media/ceb42cd2838a68f02e27032469c01870.jpg" />
+            <div className="introduction">
+                <h2>Begin Your Journey</h2>
+                <p>We belive nobody should be left behind. Because of this, we help peoplewho are helping people.
+                    <br />Upstream's impact is greatest when we work as ONE.  </p>
+            </div>
         </div>
     )
 }
