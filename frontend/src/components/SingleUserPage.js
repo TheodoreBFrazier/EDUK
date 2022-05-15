275c5f6e94dec67974c3aa3b9ddaef4353d8537f
Nima sherpa <Toplouie78@gmail.com>
2022-05-15 17:55:17 -0400

    merge fix

--- frontend/src/components/SingleUserPage.js
+++ frontend/src/components/SingleUserPage.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-// import { Link } from "react-router-dom";
-=======
 import React from "react";
 import { Link } from "react-router-dom";
->>>>>>> 734a0cb51291446e016f320c002db8c2f58fc516
 
 // function SingleUser({ user }) {
 //     return (
