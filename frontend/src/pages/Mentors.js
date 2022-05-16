e0665d2a9b6b3b34ae00e4d104c63a895c8693d6
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-15 21:55:04 -0400

    Mentors page commit

---
+++ frontend/src/pages/Mentors.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+function MentorsPages () {
+    return (
+        <div>
+            Mentors
+        </div>
+    )
+}
+
+export default MentorsPages;
