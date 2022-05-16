000447b8cf3da7bd344b737d0482780653226a5c
Nima sherpa <Toplouie78@gmail.com>
2022-05-16 17:55:23 -0400

    simple nav

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,35 +1,36 @@
-import React from "react"
-
+import React from "react";
 //importing Link function
-
-import { Link } from "react-router-dom"
-
-
+import { Link } from "react-router-dom";
 // importing button from material UI
-
-import { Button } from "@mui/material"
-
+import { Button } from "@mui/material";
+import "./NavBar.css";
+// import DropDown from "./Dropdown";
+import logo from "../images/logo.png";
+// login Icon
+import LoginIcon from "@mui/icons-material/Login";
 
 export default function NavBar() {
-    return (
-        <nav className="navigation">
-            <Link to="/">
-                <h3> Home </h3>
-            </Link>
-            <Link to="/resources">
-                <h3> Resources </h3>
-            </Link>
-            <Link to="/about">
-                <h3> About </h3>
-            </Link>
-            <Link to="/users/create">
-                <h3> Users </h3>
-            </Link>
-
-            <Button variant='contained'>
-                <Link to="/users"> User Portal</Link>
-            </Button>
-        </nav>
-    )
+	return (
+		<nav>
+			<div>
+				<Link to="/">
+					<img className="logoimg" src={logo} alt="logo" />
+				</Link>
+			</div>
+			<div className="navLinks">
+				<Link to="/resources">
+					<h3> Resources </h3>
+				</Link>
+				<Link to="/about">
+					<h3> About </h3>
+				</Link>
+				<Link to="/users/create">
+					<h3> Users </h3>
+				</Link>
+				<Link to="/users">
+					<LoginIcon className="loginIcon" fontSize="large"></LoginIcon>
+				</Link>
+			</div>
+		</nav>
+	);
 }
-
