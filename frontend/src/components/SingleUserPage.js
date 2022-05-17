f48ae8bc26bbed13fbc9264e11ba701b29427977
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-05-16 21:59:12 -0400

    working on similated login monday

--- frontend/src/components/SingleUserPage.js
+++ frontend/src/components/SingleUserPage.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 // import React from "react";
->>>>>>> 7f6d6b66e8fb1c52e4d2ed3393d450061ce13e54
 // import { Link } from "react-router-dom";
 
 // function SingleUser({ user }) {
