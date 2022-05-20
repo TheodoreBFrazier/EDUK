6f6106b0dedc98e77b9afdfce5d92a2e8a91230f
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-20 10:25:06 -0400

    tidying up Resources CSS

--- frontend/src/pages/ResourcesPage.js
+++ frontend/src/pages/ResourcesPage.js
@@ -10,18 +10,17 @@ function ResourcesPage() {
 			<div className="resources-intro">
 				<br />
 				<br />
-				<h2 className="resources-h2">Find Your Calling</h2>
-				<h4>
-					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-					nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in{" "}
-					<br />
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa <br />
-					qui officia deserunt mollit anim id est laborum."
-				</h4>
+				<div className="resources-intro-text">
+					<h2 className="resources-h2">Find Your Calling</h2>
+					<p className="resources-p">
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+						nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.
+						<br />
+						<br />
+					</p>
+				</div>
 			</div>
 			<div className="search-resource">
 				<SearchBar />
