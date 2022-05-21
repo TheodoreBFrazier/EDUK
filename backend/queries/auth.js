63e22187fa8e2a5fbd4503b56095e5af64ca1354
Mukayila ALAO <mukayilalao@yahoo.fr>
2022-05-21 10:41:04 -0400

    using bcrypt to hash the password

--- backend/queries/auth.js
+++ backend/queries/auth.js
@@ -1,15 +1,23 @@
 const db = require("../db/dbConfig.js");
+const bcrypt = require("bcrypt");
 
-
-const authUser = async(user_name, password) => {
-    try {
-        const uid = await db.one("SELECT uid FROM users WHERE user_name=$1 AND password=$2",[user_name, password]);
-        return uid;
-    } catch (error) {
-        return error;
-    }
-}
+const authUser = async (user_name, password) => {
+  try {
+    const user = await db.one(
+      "SELECT * FROM users WHERE user_name=$1",
+      user_name
+    );
+    bcrypt.compare(password, user.password, (error, isCorrect) => {
+      if (error) {
+        return new Error("Invalid password!!");
+      }
+      return user.uid;
+    });
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    authUser
+  authUser,
 };
