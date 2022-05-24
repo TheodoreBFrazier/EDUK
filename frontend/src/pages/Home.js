db055305bc080745fbf55d3c157f4ba0e4afd447
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-24 12:02:21 -0400

    Building a hover effect for Join us to become a mentor button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Background from "../images/home.jpg";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+
 
 
 function Home() {
@@ -14,40 +14,40 @@ function Home() {
 				</div>
 				<div className="joinUs">
 					<div>
-						<h3>Join us become a</h3>
-					</div>
-
-					<div>
-						<Link to="/mentors/create">
-							<Button color="secondary" variant="text" size="large">
-								Mentor
-							</Button>
-						</Link>
+						<div>
+							<Link to="/mentors/create">
+								<button>
+									<h3>Join us become a Mentor</h3>
+								</button>
+							</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="introduction">
-				<h2>Begin Your Journey</h2>
-				<div className="intro-container">
-					<img
-						alt="homeimg"
-						src="https://nces.ed.gov/images/homepage/coe.jpg"
-					/>
-					<p>
-						The goal of this app is to connect highschool, college students,
-						minority groups and residents of underserved communities to
-						resources that they wouldn’t have access to otherwise. Whether it be
-						career tips , scholarships , grants, continued learning, programs,
-						etc, We hope to bridge the the knowledge gap within these
-						underserved communities. We want to eliminate the gatekeeping of
-						much needed information and create a hub/gateway where people can
-						easily find resources they desperately need to improve their quality
-						of life.
-					</p>
+				<div className="introduction">
+					<h2>Begin Your Journey</h2>
+					<div className="intro-container">
+						<img
+							alt="homeimg"
+							src="https://nces.ed.gov/images/homepage/coe.jpg"
+						/>
+						<p>
+							The goal of this app is to connect highschool, college students,
+							minority groups and residents of underserved communities to
+							resources that they wouldn’t have access to otherwise. Whether it be
+							career tips , scholarships , grants, continued learning, programs,
+							etc, We hope to bridge the the knowledge gap within these
+							underserved communities. We want to eliminate the gatekeeping of
+							much needed information and create a hub/gateway where people can
+							easily find resources they desperately need to improve their quality
+							of life.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
+
+
 }
 
 export default Home;
