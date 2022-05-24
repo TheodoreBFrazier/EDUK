026bd45c0fd5a7f993edeb65edd539a4434a0104
Nima sherpa <Toplouie78@gmail.com>
2022-05-24 17:25:33 -0400

    check box working to add resource

--- frontend/src/components/CreateResource.js
+++ frontend/src/components/CreateResource.js
@@ -5,89 +5,142 @@ import { useState } from "react";
 
 const API = process.env.REACT_APP_API_URL;
 function CreateResource() {
-  const [resource, setResource] = useState({
-    resource_name: "",
-    description: "",
-    resource_category: "",
-    start_datetime: "",
-    end_datetime: "",
-    url: "",
-    is_verified: false,
-  });
+	// check box
+	const [highSchool, setHighSchool] = useState(false);
+	const [college, setCollege] = useState(false);
+	const [continueLearning, setContinueLearning] = useState(false);
 
-  const navigate = useNavigate();
-  const handleTextChange = (e) => {
-    setResource({ ...resource, [e.target.id]: e.target.value });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //add a new resource to the database
+	// all state to send
+	const [resource, setResource] = useState({
+		resource_name: "",
+		description: "",
+		resource_category: "",
+		resourcefor: [],
+		start_datetime: "",
+		end_datetime: "",
+		url: "",
+		is_verified: false,
+	});
 
-    axios
-      .post(`${API}/resources`, resource)
-      .then((res) => navigate("/resources"))
-      .catch((e) => console.log(e));
-  };
-  return (
-    <div>
-      <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="resource_name">Resource Name</label>
-        <br />
-        <input
-          id="resource_name"
-          value={resource.resource_name}
-          onChange={handleTextChange}
-          type="text"
-        />
-        <br />
-        <label htmlFor="description">Resource Description</label>
-        <br />
-        <textarea
-          id="description"
-          value={resource.description}
-          onChange={handleTextChange}
-        />
-        <br />
-        <label htmlFor="resource_category">Resource Category</label>
-        <br />
-        <input
-          id="resource_category"
-          value={resource.resource_category}
-          onChange={handleTextChange}
-          type="text"
-        />
-        <br />
-        <label htmlFor="start_datetime">Start Date and Time</label>
-        <br />
-        <input
-          id="start_datetime"
-          value={resource.start_datetime}
-          onChange={handleTextChange}
-          type="text"
-        />
-        <br />
-        <label htmlFor="end_datetime">End Date and Time</label>
-        <br />
-        <input
-          id="end_datetime"
-          value={resource.end_datetime}
-          onChange={handleTextChange}
-          type="text"
-        />
-        <br />
-        <label htmlFor="url">Link</label>
-        <br />
-        <input
-          id="url"
-          value={resource.url}
-          onChange={handleTextChange}
-          type="text"
-        />
-        <br />
-        <input type="submit" />
-      </form>
-    </div>
-  );
+	if (highSchool) {
+		resource.resourcefor[0] = "highschool";
+	} else {
+		resource.resourcefor[0] = "null";
+	}
+	if (college) {
+		resource.resourcefor[1] = "college";
+	} else {
+		resource.resourcefor[1] = "null";
+	}
+	if (continueLearning) {
+		resource.resourcefor[2] = "continue learning";
+	} else {
+		resource.resourcefor[2] = "null";
+	}
+
+	const navigate = useNavigate();
+	const handleTextChange = (e) => {
+		setResource({ ...resource, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		//add a new resource to the database
+		axios
+			.post(`${API}/resources`, resource)
+			.then((res) => navigate("/resources"))
+			.catch((e) => console.log(e));
+	};
+
+	return (
+		<div>
+			<form className="form" onSubmit={handleSubmit}>
+				<label htmlFor="resource_name">Resource Name</label>
+				<br />
+				<input
+					id="resource_name"
+					value={resource.resource_name}
+					onChange={handleTextChange}
+					type="text"
+				/>
+				<br />
+				<label htmlFor="description">Resource Description</label>
+				<br />
+				<textarea
+					id="description"
+					value={resource.description}
+					onChange={handleTextChange}
+				/>
+				<br />
+				<label htmlFor="resource_category">Resource Category</label>
+				<br />
+				<input
+					id="resource_category"
+					value={resource.resource_category}
+					onChange={handleTextChange}
+					type="text"
+				/>
+				<br />
+
+				{/* check box  */}
+
+				<label htmlFor="highschool">High School</label>
+				<input
+					id="highschool"
+					value="highschool"
+					type="checkbox"
+					onChange={(e) => setHighSchool(e.target.checked)}
+				/>
+
+				<label htmlFor="college">College</label>
+				<input
+					id="college"
+					type="checkbox"
+					name="college"
+					onChange={(e) => setCollege(e.target.checked)}
+				/>
+
+				<label htmlFor="continueLearning">Continue Learning</label>
+				<input
+					id="continueLearning"
+					type="checkbox"
+					name="continueLearning"
+					onChange={(e) => setContinueLearning(e.target.checked)}
+				/>
+
+				{/* check box  */}
+
+				<label htmlFor="start_datetime">Start Date and Time</label>
+				<br />
+				<input
+					id="start_datetime"
+					value={resource.start_datetime}
+					onChange={handleTextChange}
+					type="text"
+				/>
+				<br />
+				<label htmlFor="end_datetime">End Date and Time</label>
+				<br />
+				<input
+					id="end_datetime"
+					value={resource.end_datetime}
+					onChange={handleTextChange}
+					type="text"
+				/>
+				<br />
+				<label htmlFor="url">Link</label>
+				<br />
+				<input
+					id="url"
+					value={resource.url}
+					onChange={handleTextChange}
+					type="text"
+				/>
+				<br />
+				<input type="submit" />
+			</form>
+		</div>
+	);
 }
 
 export default CreateResource;
