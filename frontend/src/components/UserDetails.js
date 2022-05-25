5fd23319fd31978e09eb18b3e908239887fefe26
David Vidals <david.vidals08@gmail.com>
2022-05-25 12:39:58 -0400

    minor change

--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -41,7 +41,7 @@ function UserDetails() {
       <div className="welcome">
         <aside className="profile-card">
           <header>
-            <a href="#">
+            <a href="#!">
               <img
                 src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
                 alt="profile-img"
