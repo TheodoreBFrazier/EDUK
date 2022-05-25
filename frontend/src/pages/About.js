76778ccffadb4a01910f3e1bcc29ea65027582b7
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-25 11:58:28 -0400

    Change yellow hover Button to the green in the theme David picked

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,7 +4,7 @@ import { Card } from "@mui/material";
 
 var cardStyle = {
 	height: "600px",
-	width: "400px",
+	width: "1000px",
 	padding: "20px",
 	boxshadow: "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px",
 	// display: "inline-block",
@@ -13,7 +13,7 @@ var cardStyle = {
 function About() {
 	return (
 		<div className="about">
-			<div className="about-container-one">
+			
 				<div className="valueNmission">
 					<div>
 						<h2 id="our_values">Our Values</h2>
@@ -24,8 +24,9 @@ function About() {
 						extend grace to ourselves and others. We accelerate each other and
 						are accountable to each other to deliver on our vision.
 					</div>
-					{/* <img src="https://blog.vantagecircle.com/content/images/2019/07/company-core-values.png" alt="value" /> */}
 				</div>
+
+
 
 				<div className="valueNmission">
 					<div>
@@ -55,11 +56,7 @@ function About() {
 					start creating and helping people. I hope to once day create an
 					app/website that can help under served communities in New York. A
 					fun Fact about me is that I love play video games.
-					<img
-						src="https://img.etimg.com/thumb/msid-88634316,width-525,height-393,imgsize-65126,resizemode-8,quality-100/tech/technology/tracking-the-buzz-in-tech.jpg"
-						width="500px"
-						alt="David"
-					/>
+	
 				</Card>
 
 				<Card style={cardStyle} variant="outlined">
@@ -69,11 +66,7 @@ function About() {
 					problems by putting myself in the users’ shoes and providing a
 					solution. The ability to be creative in my work is what truly
 					empowers me to move forward and keep learning as an engineer.
-					<img
-						src="https://www.investopedia.com/thmb/TID4EokOecUZzUw8N-YpAsj7BAI=/1885x1414/smart/filters:no_upscale()/GettyImages-964033964-ca3290057ccc4024b57e755423572264.jpg"
-						width="500px"
-						alt="Lili"
-					/>
+		
 				</Card >
 
 
@@ -86,11 +79,7 @@ function About() {
 					maintenance engineer. I will finish by saying that technology is
 					driving our lives today and I can't wait to land my first job and
 					contribute.
-					<img
-						src="https://www.investopedia.com/thmb/TID4EokOecUZzUw8N-YpAsj7BAI=/1885x1414/smart/filters:no_upscale()/GettyImages-964033964-ca3290057ccc4024b57e755423572264.jpg"
-						width="500px"
-						alt="Lili"
-					/>
+
 				</Card >
 
 
@@ -100,11 +89,7 @@ function About() {
 					implementing new ideas . I am always seeking to expand my
 					knowledge on tech , Ability to learn and implement new
 					technologies quickly.
-					<img
-						src="https://res.cloudinary.com/people-matters/image/upload/fl_immutable_cache,w_624,h_351,q_auto,f_auto/v1578710070/1578710068.jpg"
-						width="500px"
-						alt="Nima"
-					/>
+	
 				</Card >
 
 
@@ -116,17 +101,11 @@ function About() {
 					fellowship with a 9% acceptance rate whose graduates have been
 					hired as developers at leading companies such as Pinterest, JP
 					Morgan, and Spotify.
-
-					<img
-						src="https://image.cnbcfm.com/api/v1/image/105510591-1539718864161tech-drivers-mezz-2018.jpg?v=1539718880"
-						width="500px"
-						alt="Theodore"
-					/>
 				</Card >
 
 
 
-			</div>
+			
 		</div>
 	);
 }
