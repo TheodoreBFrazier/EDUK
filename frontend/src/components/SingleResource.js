c3e6e774ac28a499d277aec1b29f971ba9267471
Nima sherpa <Toplouie78@gmail.com>
2022-05-27 20:40:39 -0400

    merge

--- frontend/src/components/SingleResource.js
+++ frontend/src/components/SingleResource.js
@@ -9,47 +9,47 @@ import { Card } from "@mui/material";
 import GeneralSuccessM from "./GeneralSuccessM";
 
 var cardStyle = {
-  height: "auto",
-  width: "400px",
-  padding: "20px",
-  boxshadow: "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px",
+	height: "auto",
+	width: "400px",
+	padding: "20px",
+	boxshadow: "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px",
 };
 
 //need to add a boolean in order to not show delete button on resources page
 
 function SingleResource({ resource, showDelete, removeResource, showMessage }) {
-  return (
-    <div className="resource-card">
-      {showMessage ? (
-        <GeneralSuccessM message={"Deleted Succesfully!!!"} />
-      ) : (
-        ""
-      )}
-      <Card style={cardStyle} variant="outlined">
-        <Link to={`/resources/${resource.resource_id}`}>
-          <h4> {resource.resource_name} </h4>
-        </Link>
-        <p> {resource.description} </p>
-        <Link to={`/resources/${resource.resource_id}`}>
-          <Button variant="text" size="small">
-            View
-          </Button>
-        </Link>
-        {showDelete ? (
-          <Button
-            color="error"
-            variant="contained"
-            size="small"
-            onClick={() => removeResource(resource.resource_id)}
-          >
-            Delete
-          </Button>
-        ) : (
-          ""
-        )}
-      </Card>
-    </div>
-  );
+	return (
+		<div className="resource-card">
+			{showMessage ? (
+				<GeneralSuccessM message={"Deleted Succesfully!!!"} />
+			) : (
+				""
+			)}
+			<Card style={cardStyle} variant="outlined">
+				<Link to={`/resources/${resource.resource_id}`}>
+					<h4> {resource.resource_name} </h4>
+				</Link>
+				<p> {resource.description} </p>
+				<Link to={`/resources/${resource.resource_id}`}>
+					<Button variant="text" size="small">
+						View
+					</Button>
+				</Link>
+				{showDelete ? (
+					<Button
+						color="error"
+						variant="contained"
+						size="small"
+						onClick={() => removeResource(resource.resource_id)}
+					>
+						Delete
+					</Button>
+				) : (
+					""
+				)}
+			</Card>
+		</div>
+	);
 }
 
 export default SingleResource;
