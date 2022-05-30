a8bb65fa9a05e773524e538a563a26e1035cc0b8
Nima sherpa <Toplouie78@gmail.com>
2022-05-30 19:30:02 -0400

    create resource css

--- frontend/src/components/UserResource.js
+++ frontend/src/components/UserResource.js
@@ -6,39 +6,39 @@ import Button from "@mui/material/Button";
 import { Card } from "@mui/material";
 
 var cardStyle = {
-  height: "auto",
-  width: "400px",
-  padding: "20px",
-  boxshadow: "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px",
+	height: "auto",
+	width: "400px",
+	padding: "20px",
+	boxshadow: "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px",
 };
 
 //need to add a boolean in order to not show delete button on resources page
 
 function UserResource({ resource, removeResource }) {
-  return (
-    <div className="resource-card">
-      <Card style={cardStyle} variant="outlined">
-        <Link to={`/resources/${resource.resource_id}`}>
-          <h4> {resource.resource_name} </h4>
-        </Link>
-        <p> {resource.description} </p>
-        <Link to={`/resources/${resource.resource_id}`}>
-          <Button variant="text" size="small">
-            View
-          </Button>
-        </Link>
+	return (
+		<div className="resource-card">
+			<Card style={cardStyle} variant="outlined">
+				<Link to={`/resources/${resource.resource_id}`}>
+					<h4> {resource.resource_name} </h4>
+				</Link>
+				<p> {resource.description} </p>
+				<Link to={`/resources/${resource.resource_id}`}>
+					<Button variant="contained" size="small">
+						View
+					</Button>
+				</Link>
 
-        <Button
-          color="error"
-          variant="contained"
-          size="small"
-          onClick={() => removeResource(resource.resource_id)}
-        >
-          Delete
-        </Button>
-      </Card>
-    </div>
-  );
+				<Button
+					color="error"
+					variant="contained"
+					size="small"
+					onClick={() => removeResource(resource.resource_id)}
+				>
+					Delete
+				</Button>
+			</Card>
+		</div>
+	);
 }
 
 export default UserResource;
